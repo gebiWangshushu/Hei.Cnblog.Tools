ccff17f3e3ca0644a2ec3879a7811b2fe5e2891e
wangnima <389483637@qq.com>
2022-06-27 21:32:56 +0800

    修改备注提醒

--- Hei.Cnblog.Tools/Form2.Designer.cs
+++ Hei.Cnblog.Tools/Form2.Designer.cs
@@ -37,6 +37,8 @@
             this.label3 = new System.Windows.Forms.Label();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.blogid = new System.Windows.Forms.Label();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -44,22 +46,22 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(14, 92);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(54, 20);
+            this.label1.Size = new System.Drawing.Size(62, 20);
             this.label1.TabIndex = 0;
-            this.label1.Text = "账号：";
+            this.label1.Text = "账  号：";
             // 
             // textUserName
             // 
-            this.textUserName.Location = new System.Drawing.Point(73, 89);
+            this.textUserName.Location = new System.Drawing.Point(88, 89);
             this.textUserName.Name = "textUserName";
-            this.textUserName.Size = new System.Drawing.Size(356, 27);
+            this.textUserName.Size = new System.Drawing.Size(341, 27);
             this.textUserName.TabIndex = 7;
             // 
             // textPassword
             // 
-            this.textPassword.Location = new System.Drawing.Point(73, 123);
+            this.textPassword.Location = new System.Drawing.Point(88, 123);
             this.textPassword.Name = "textPassword";
-            this.textPassword.Size = new System.Drawing.Size(356, 27);
+            this.textPassword.Size = new System.Drawing.Size(341, 27);
             this.textPassword.TabIndex = 8;
             // 
             // label2
@@ -67,13 +69,13 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(14, 126);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(54, 20);
+            this.label2.Size = new System.Drawing.Size(84, 20);
             this.label2.TabIndex = 2;
-            this.label2.Text = "密码：";
+            this.label2.Text = "访问令牌：";
             // 
             // btnComfirm
             // 
-            this.btnComfirm.Location = new System.Drawing.Point(14, 156);
+            this.btnComfirm.Location = new System.Drawing.Point(14, 196);
             this.btnComfirm.Name = "btnComfirm";
             this.btnComfirm.Size = new System.Drawing.Size(415, 29);
             this.btnComfirm.TabIndex = 9;
@@ -83,9 +85,9 @@
             // 
             // textBlogid
             // 
-            this.textBlogid.Location = new System.Drawing.Point(73, 54);
+            this.textBlogid.Location = new System.Drawing.Point(88, 54);
             this.textBlogid.Name = "textBlogid";
-            this.textBlogid.Size = new System.Drawing.Size(356, 27);
+            this.textBlogid.Size = new System.Drawing.Size(341, 27);
             this.textBlogid.TabIndex = 6;
             // 
             // label3
@@ -116,11 +118,32 @@
             this.blogid.TabIndex = 18;
             this.blogid.Text = "博客ID每个账号唯一如上是我的博客园地址，ID为：xiaxiaolu";
             // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(14, 153);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(391, 20);
+            this.linkLabel2.TabIndex = 19;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "https://i.cnblogs.com/settings#enableServiceAccess";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 173);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(415, 20);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "点击上面链接去到底部“Metaweblog访问令牌”->\"查看\"获取";
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(444, 193);
+            this.ClientSize = new System.Drawing.Size(452, 237);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.linkLabel2);
             this.Controls.Add(this.blogid);
             this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.textBlogid);
@@ -151,5 +174,7 @@
         private Label label3;
         private LinkLabel linkLabel1;
         private Label blogid;
+        private LinkLabel linkLabel2;
+        private Label label4;
     }
 }
